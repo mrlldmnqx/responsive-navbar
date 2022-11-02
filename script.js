e3bdc49ad61a812dd647214365c54c294363ff28
mrlldmnqx <mrlldmnq.dev@gmail.com>
2022-11-02 14:25:47 +0800

    added animation to toggle button

--- script.js
+++ script.js
@@ -4,4 +4,5 @@ const toggleBtn = document.querySelector(".toggle-button");
 const navList = document.querySelector(".nav-list");
 toggleBtn.addEventListener("click", () => {
   navList.toggleAttribute("toggle");
+  toggleBtn.toggleAttribute("toggle");
 });
